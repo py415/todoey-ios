8cfe8975bdbda22ff91e1bae46ec25b10c1cfd88
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-05 23:34:06 -0700

    User can now select category cell to be directed to a todo list tableview

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -16,6 +16,11 @@ class TodoListViewController: UITableViewController {
     
     // Properties
     var itemArray = [Item]()
+    var selectedCategory: Category? {
+        didSet {
+            loadItems()
+        }
+    }
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
@@ -77,6 +82,7 @@ class TodoListViewController: UITableViewController {
             
             newItem.title = textField.text!
             newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             
             self.itemArray.append(newItem)
             self.saveItems()
